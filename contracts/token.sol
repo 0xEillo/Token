272003280f11eb41605e74dec8c25893d55de6c1
Oliver-Ryall <oliryall0404@gmail.com>
2021-10-25 19:16:06 +0100

    Add Ownable contract from OpenZeppelin

--- contracts/token.sol
+++ contracts/token.sol
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.4.22 <0.9.0;
-import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 
-contract myToken is ERC20 {
-    constructor() ERC20("myToken", "OR") {
-        _mint(msg.sender, 1000);
+contract myToken is ERC20Capped, Ownable {
+    constructor() ERC20("myToken", "OR") ERC20Capped(100000) {
+        _mint(msg.sender, 10000);
     }
 }
